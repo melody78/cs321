bce2788d585f08ee1c8112cfc10359e97b55ee06
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2022-03-03 17:24:53 -0900

    add: progress before threads

--- proj3/client.cpp
+++ proj3/client.cpp
@@ -6,23 +6,41 @@
 // For CS 321 Spring 2022
 // Lab 3: Server & Client Programming
 
-#include <cstdio>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <string>
+#include <cstdio>
 #include <iostream>
 #include <ostream>
+#include <string>
+#include <sys/socket.h>
+#include <thread>
+#include <unistd.h>
 
 #define LOCALHOST "127.0.0.1"
 #define PORT 8000
 
+void readHandler(int &socket)
+{
+    char buffer[1024] = {0};
+
+    while (true)
+    {
+        auto reada = read(socket, buffer, 1024);
+        if (reada == 0)
+        {
+            std::cout << "Client disconnected." << std::endl;
+            break;
+        }
+        else
+        {
+            std::cout << reada << std::endl;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int sock = 0;
-    struct sockaddr_in server_address
-    {
-    };
+    struct sockaddr_in server_address;
 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -33,10 +51,10 @@ int main(int argc, char const *argv[])
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
+    // Convert IPv4 & IPv6 addresses: text -> binary format.
     if (inet_pton(AF_INET, LOCALHOST, &server_address.sin_addr) <= 0)
     {
-        std::cerr << "❌ Invalid address -- current address not supported." << std::endl;
+        std::cerr << "❌ Invalid address -- currently not supported." << std::endl;
         return -1;
     }
 
@@ -45,6 +63,9 @@ int main(int argc, char const *argv[])
         std::cerr << "🚫 Connection failed!" << std::endl;
         return -1;
     }
+
+    std::thread readThread(&readHandler, socket);
+    readThread.join();
 
     while (true)
     {
@@ -58,19 +79,19 @@ int main(int argc, char const *argv[])
         send(sock, sent_message.c_str(), sent_message.length(), 0);
         std::cout << "✅ Message sent." << std::endl;
 
-        // Receive the corresponding message back.
-        int message_length = 1024;
+        // // Receive the corresponding message back.
+        // int message_length = 1024;
 
-        // read(sock, &message_length, sizeof(int));
-        std::string received_message;
-        received_message.resize(message_length);
-        read(sock, &received_message[0], message_length);
-        std::cout << "Message received from server: " << received_message.c_str() << std::endl;
+        // // read(sock, &message_length, sizeof(int));
+        // std::string received_message;
+        // received_message.resize(message_length);
+        // read(sock, &received_message[0], message_length);
+        // std::cout << "Message received from server: " << received_message.c_str() << std::endl;
 
-        if (!received_message.compare("BYE") || !received_message.compare("bye"))
-        {
-            std::cout << "Client has disconnected." << std::endl;
-            return 0;
-        }
+        // if (!received_message.compare("BYE")) // FIXME: client disconnection bug.
+        // {
+        //     std::cout << "Client has disconnected." << std::endl;
+        //     return 0;
+        // }
     }
 }
